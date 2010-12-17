0db5f6d5c6feadfcb4db1830e0b85aef0aa37baa
Kazuhiro Osawa <yappo@shibuya.pl>
2010-12-17 20:59:02 +0900

    initial commit

---
+++ lib/Nigiri.pm
@@ -0,0 +1,32 @@
+package Nigiri;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Nigiri -
+
+=head1 SYNOPSIS
+
+  use Nigiri;
+
+=head1 DESCRIPTION
+
+Nigiri is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
